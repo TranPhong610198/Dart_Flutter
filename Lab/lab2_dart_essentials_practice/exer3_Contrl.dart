c7547c6538af29e91a9d1366148fd2a754bbcf87
-THEMORIARTY- <tphong610198@gmail.com>
2026-01-21 10:41:45 +0700

    switch/case update

--- Lab/lab2_dart_essentials_practice/exer3_Contrl.dart
+++ Lab/lab2_dart_essentials_practice/exer3_Contrl.dart
@@ -1,4 +1,6 @@
 
+import 'dart:io';
+
 void main() {
   int score = 8;
   // if/esle block
@@ -10,5 +12,33 @@ void main() {
     print('this is a good score');
 
   // switch/case 
-  
+  print("Input day of week: ");
+  String? day = stdin.readLineSync();
+  switch (day?.toLowerCase()) {
+    case 'monday':
+      print("Mao phắc lại thứ 2 rồi, DCMMMMM~~~");
+      break;
+    case 'tuesday':
+      print("Mao phắc lại thứ 3 rồi, DCMMMMM~~~");
+      break;
+    case 'wednesday':
+      print("Mao phắc lại thứ 4 rồi, DCMMMMM~~~");
+      break;
+    case 'thurday':
+      print("Mao phắc lại thứ 5 rồi, DCMMMMM~~~");
+      break;
+    case 'friday':
+      print("Mao phắc lại thứ 6 rồi, DCMMMMM~~~");
+      break;
+    case 'saturday':
+      print("Mao phắc lại thứ 7 rồi, DCMMMMM~~~");
+      break;
+    case 'sunday':
+      print("Mao phắc lại Chủ Nhật rồi, DCMMMMM~~~");
+      break;
+    default:
+      print("Dell mịa có cái thứ cũng nhập sai, làm đc việc ko ??!!!");
+  }
+
+
 }
