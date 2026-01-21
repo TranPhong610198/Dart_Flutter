0995fdc20ac9b075d105d488696a8107d999a0e6
-THEMORIARTY- <tphong610198@gmail.com>
2026-01-21 23:14:10 +0700

    complete login form super basic

---
+++ Flutter/prm393_buoi5_login/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '/screens/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // MaterialApp cung cấp Directionality, Theme, Navigator...
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
---
+++ Flutter/prm393_buoi5_login/lib/screens/login.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 56.0,
+            left: 24.0,
+            bottom: 24.0,
+            right: 24.0,
+          ),
+          child: Column(
+            children: [
+              /// Logo title and subtitle
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                    height: 150,
+                    image: AssetImage('assets/images/img.png'),
+                  ),
+                  Text('loginTitle', textAlign: TextAlign.center),
+                  const SizedBox(height: 8.0),
+                  Text('loginSubTitle', textAlign: TextAlign.center),
+                ],
+              ),
+
+              /// Form
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 32.0),
+                  child: Column(
+                    children: [
+                      ///Email
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.subdirectory_arrow_right),
+                          labelText: 'Email',
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+
+                      ///Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.password),
+                          labelText: 'Password',
+                          suffixIcon: Icon(Icons.panorama_fish_eye),
+                        ),
+                      ),
+                      const SizedBox(height: 8.0),
+
+                      ///Remember Me & Forget Password
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          /// Remember
+                          Row(
+                            children: [
+                              Checkbox(value: true, onChanged: (value) {}),
+                              const Text("Remember Me"),
+                            ],
+                          ),
+
+                          ///Forget Password
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text("Forget Password ?"),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16.0),
+
+                      /// Sign in button
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Sign in"),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+
+                      /// Create Account Button
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: Text("Register"),
+                        ),
+                      ),
+                      // const SizedBox(height: 32.0,),
+                    ],
+                  ),
+                ),
+              ),
+
+              /// Divider
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      indent: 60,
+                      endIndent: 5,
+                    ),
+                  ),
+                  Text(
+                    "or sign in with",
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  Flexible(
+                    child: Divider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      indent: 5,
+                      endIndent: 60,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32.0,),
+
+              /// Footer
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.flutter_dash),
+                    ),
+                  ),
+                  const SizedBox(width: 16.0,),
+
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.facebook),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
