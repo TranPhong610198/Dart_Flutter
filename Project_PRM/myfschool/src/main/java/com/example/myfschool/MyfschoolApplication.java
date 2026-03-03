bfe2feb7abaef7db46bf4002466f36258064fca4
-THEMORIARTY- <tphong610198@gmail.com>
2026-03-04 01:20:28 +0700

    init project

---
+++ Project_PRM/myfschool/src/main/java/com/example/myfschool/MyfschoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfschoolApplication.class, args);
+	}
+
+}
